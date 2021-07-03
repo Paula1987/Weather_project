2012932b857389a8a5c5ca9ba3ac6f9f5b7d741a
Paula Cortiñas <paula_cortinas@hotmail.com>
2021-07-02 23:02:15 -0300

    Add to do list

--- weather_forecast.py
+++ weather_forecast.py
@@ -2,3 +2,9 @@ import requests
 
 r = requests.get('https://www.metaweather.com/api/location/2455920')
 
+#TO DO:
+# 1) Ask user what city they are in.
+# 2) Get city name to get WOEID.
+# 3) Use WOEID to get weather data.
+# 4) Convert data from JSON text to python dictionary.
+# 5) Loop over the dictionary and display the forecast for each day.
