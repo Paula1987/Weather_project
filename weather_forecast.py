5b60ca90212d90fcafa885feb078d5d9bec10895
Paula Cortiñas <paula_cortinas@hotmail.com>
2021-07-02 23:32:41 -0300

    Ask user location

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,10 +1,12 @@
+location = input('Where in the world are you?')
+
 import requests
 
-r = requests.get('https://www.metaweather.com/api/location/2455920')
+r = requests.get('https://www.metaweather.com/api/location/search/?query')
 
 #TO DO:
 # 1) Ask user what city they are in.
-# 2) Get city name to get WOEID.
+# 2) Get city name to get WOEID(where on earth ID).
 # 3) Use WOEID to get weather data.
 # 4) Convert data from JSON text to python dictionary.
 # 5) Loop over the dictionary and display the forecast for each day.
